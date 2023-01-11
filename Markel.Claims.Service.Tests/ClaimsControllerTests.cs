d2e47ba2bd2e354618f986f031c4862e6a88ce28
senthil kandasamy <senthil.ak@gmail.com>
2023-01-11 21:23:08 +0000

    fix: add missing testclaims method

--- Markel.Claims.Service.Tests/ClaimsControllerTests.cs
+++ Markel.Claims.Service.Tests/ClaimsControllerTests.cs
@@ -21,20 +21,33 @@ namespace Markel.Claims.Service.Tests
         public async void AssertClaimsReturnedThen_RespondWithClaims()
         {
             //Arrange
-            //
             claimsRepo = new Mock<IGenericRepository<Markel.Claims.Service.Data.Claims>>();
             claimsRepo.Setup(repo => repo.GetAll()).ReturnsAsync(GetTestClaims());
 
             //Act
             ClaimsController controller = new ClaimsController(claimsRepo.Object);
 
-            //Assert
             var result = controller.Get();
             Assert.IsType<List<Markel.Claims.Service.Data.Claims>>(result.Result);
             List<Markel.Claims.Service.Data.Claims> claimsList = result.Result.ToList();
             Assert.True(claimsList.Count == 2);
         }
 
-        
+        private IReadOnlyList<Markel.Claims.Service.Data.Claims> GetTestClaims()
+        {
+            List<Markel.Claims.Service.Data.Claims> claimsList = new List<Markel.Claims.Service.Data.Claims>();
+
+            Markel.Claims.Service.Data.Claims claims1 = new Markel.Claims.Service.Data.Claims();
+            claims1.ClaimId = 1;
+            claims1.AssuredName = "Test1";
+            claimsList.Add(claims1);
+
+            Markel.Claims.Service.Data.Claims claims2 = new Markel.Claims.Service.Data.Claims();
+            claims1.ClaimId = 2;
+            claims1.AssuredName = "Test2";
+            claimsList.Add(claims2);
+
+            return claimsList;
+        }
     }
 }
