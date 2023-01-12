ce92daad491d769e39162a073ccff16b78d6d471
senthil kandasamy <senthil.ak@gmail.com>
2023-01-12 08:26:25 +0000

    feat: add company table

--- Markel.Claims.Service.Data/Database/DatabaseBootstrap.cs
+++ Markel.Claims.Service.Data/Database/DatabaseBootstrap.cs
@@ -24,20 +24,40 @@ namespace Markel.Claims.Service.Data
 
                 var table = connection.Query<string>("SELECT name FROM sqlite_master WHERE type='table' AND name = 'Claims';");
                 bool bTableExists = table.Any(row => row.ToLower().Contains("claims"));
-                if (bTableExists)
-                    return;
-
+                if (!bTableExists)
+                {
                 connection.Execute("Create Table Claims (" +
                                     "ClaimId INTEGER PRIMARY KEY AUTOINCREMENT, " +
                                     "UCR VARCHAR(20), " +
-                                    "CompanyId INTEGER," + 
-                                    "ClaimDate DATETIME,"+
-                                    "LossDate DATETIME, "+
-                                    "[Assured Name] VARCHAR(100), "+
-                                    "[Incurred Loss] DECIMAL(15, 2), "+
+                                    "CompanyId INTEGER," +
+                                    "ClaimDate DATETIME," +
+                                    "LossDate DATETIME, " +
+                                    "[Assured Name] VARCHAR(100), " +
+                                    "[Incurred Loss] DECIMAL(15, 2), " +
                                     "Closed BIT" +
                                     ")"
                                     );
-            }
+    
+                }
+
+            var companyTable = connection.Query<string>("SELECT name FROM sqlite_master WHERE type='table' AND name = 'Company';");
+            bTableExists = table.Any(row => row.ToLower().Contains("company"));
+            if (bTableExists)
+                return;
+
+            connection.Execute("Create Table Company (" +
+                                    "Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "Name VARCHAR(200), " +
+                                    "Address1 varchar(100)," +
+                                    "Address2 varchar(100)," +
+                                    "Address3 varchar(100), " +
+                                    "Postcode varchar(50), " +
+                                    "Country varchar(50), " +
+                                    "Active BIT" +
+                                    "InsuranceEndDate DATETIME " +
+                                    ")"
+                                    );
+            connection.Close();
+        }
     }
 }
